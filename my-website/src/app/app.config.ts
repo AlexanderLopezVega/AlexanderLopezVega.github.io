bff5c39eebd776d299b620b455f96293fde0bf18
AlexanderLopezVega <alex.lopez.ve@gmail.com>
2026-06-21 21:05:35 +0200

    Add TaigaUi

--- my-website/src/app/app.config.ts
+++ my-website/src/app/app.config.ts
@@ -1,12 +1,9 @@
+import { provideTaiga } from '@taiga-ui/core';
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration()
-  ]
+  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes), provideTaiga()],
 };
--- my-website/src/app/app.ts
+++ my-website/src/app/app.ts
@@ -1,12 +1,11 @@
+import { TuiRoot } from '@taiga-ui/core';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TuiRoot],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
-export class App {
-  protected readonly title = signal('my-website');
-}
+export class App {}
