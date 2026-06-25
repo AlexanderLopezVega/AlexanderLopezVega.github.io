b5132aee46f4f6a3b61905dfc55fb059c651a77d
AlexanderLopezVega <alex.lopez.ve@gmail.com>
2026-06-25 12:47:24 +0200

    Update project structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
-import { TuiRoot } from '@taiga-ui/core';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TuiRoot],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss',
+  styleUrl: './app.scss'
 })
-export class App {}
+export class App {
+  protected readonly title = signal('my-website');
+}
